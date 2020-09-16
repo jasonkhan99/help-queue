dc0dd5a8ce1dcd852b710f116af33903b89d5722
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-16 10:01:22 -0700

    add logic for form visible reducer

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -5,4 +5,6 @@ describe("formVisibleReducer", () => {
   test('Should return default state if no action type is recognized', () => {
     expect(formVisibleReducer(false, { type: null })).toEqual(false);
   });
+
+  
 });
