fcaa9b5abe4f5f77c980efc725a1fc8f4d353f00
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-16 10:21:29 -0700

    add test for form visible default

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -18,4 +18,8 @@ describe("rootReducer", () => {
     expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, { type: null }));
   });
 
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
 });
